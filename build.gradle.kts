40fc64b208c791a2f81c1d27409e1197ad1ded74
charls.kim <charls.kim.dev@gmail.com>
2020-04-09 18:40:12 +0900

    Update build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,15 +39,13 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("network.piction.example:entities:1.0.0")
+	implementation("network.piction.example:entities:1.0.1")
 
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 
 	runtimeOnly("com.h2database:h2")
 
-	testImplementation("org.springframework.boot:spring-boot-starter-test") {
-		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
-	}
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<Test> {
