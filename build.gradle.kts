7d6445de3cd52fad2b9d9af3c5c11b2e764a4cb5
charls.kim <charls.kim.dev@gmail.com>
2020-04-09 12:56:00 +0900

    Initial Project

---
+++ build.gradle.kts
@@ -0,0 +1,62 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.2.6.RELEASE"
+	id("io.spring.dependency-management") version "1.0.9.RELEASE"
+	id("maven-publish")
+	kotlin("jvm") version "1.3.71"
+	kotlin("plugin.spring") version "1.3.71"
+	kotlin("plugin.jpa") version "1.3.71"
+}
+
+group = "network.piction.example"
+version = "1.0.0"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+val developmentOnly by configurations.creating
+configurations {
+	runtimeClasspath {
+		extendsFrom(developmentOnly)
+	}
+}
+
+repositories {
+	jcenter()
+	mavenCentral()
+	maven {
+		name = "PictionExampleGithubPackage"
+		url = uri("https://maven.pkg.github.com/piction-protocol/entity-example")
+		credentials {
+			username = findProperty("RESOURCES_USERNAME")?.toString()
+			password = findProperty("RESOURCES_TOKEN")?.toString()
+		}
+	}
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("network.piction.example:entities:1.0.0")
+
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+	runtimeOnly("com.h2database:h2")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test") {
+		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "1.8"
+	}
+}
